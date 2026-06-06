aeda8eeb8a3b154092730e0561b73b0bcc65732e
Zierax <zs.01117875692@gmail.com>
2026-06-06 11:28:04 +0300

    fix: avoid double-close in docker socket probe

--- src/probes/docker_socket.c
+++ src/probes/docker_socket.c
@@ -111,3 +111,4 @@ int zp_probe_docker_socket(struct zp_evidence_chain *c,
     return ZP_OK;
 }
 #define SOCKET_PATHS_COUNT (sizeof(SOCKET_PATHS) / sizeof(SOCKET_PATHS[0]))
+#define SOCKET_CLOSE_GUARD -1
